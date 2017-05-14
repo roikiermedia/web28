189b3006fa7c293ffe21a95a231f514803c148d0
Thorben Dittmar <roikiermedia@users.noreply.github.com>
2017-05-14 22:09:57 +0200

    Change FastLED examples through web server

--- web28.ino
+++ web28.ino
@@ -17,6 +17,8 @@ CRGB leds[NUM_LEDS];
 
 ESP8266WebServer server(80);
 
+uint8_t gCurrentPatternNumber = 0; // Index number of which pattern is current
+
 
 void returnOK() {
   server.send(200, "text/plain", "");
@@ -47,8 +49,32 @@ void setup() {
   }
 
   server.on("/", [](){
-    FastLED.showColor(0x0000FF);
+    FastLED.showColor(0xFFFFFF);
     server.send(200, "text/html", "Hello World!");
+  });
+  server.on("/preset/0", [](){
+    gCurrentPatternNumber = 0;
+    server.send(200, "text/html", "Preset 0");
+  });
+  server.on("/preset/1", [](){
+    gCurrentPatternNumber = 1;
+    server.send(200, "text/html", "Preset 1");
+  });
+  server.on("/preset/2", [](){
+    gCurrentPatternNumber = 2;
+    server.send(200, "text/html", "Preset 2");
+  });
+  server.on("/preset/3", [](){
+    gCurrentPatternNumber = 3;
+    server.send(200, "text/html", "Preset 3");
+  });
+  server.on("/preset/4", [](){
+    gCurrentPatternNumber = 4;
+    server.send(200, "text/html", "Preset 4");
+  });
+  server.on("/preset/5", [](){
+    gCurrentPatternNumber = 5;
+    server.send(200, "text/html", "Preset 5");
   });
   server.begin();
 
@@ -64,15 +90,18 @@ void setup() {
   }
 }
 
+typedef void (*SimplePatternList[])();
+SimplePatternList gPatterns = { rainbow, rainbowWithGlitter, confetti, sinelon, juggle, bpm };
+
 uint8_t gHue = 0; // rotating "base color" used by many of the patterns
 
 void loop() {
   server.handleClient();
   
-  //confetti();
-  //FastLED.show();
-  //FastLED.delay(1000/FRAMES_PER_SECOND);
-  //EVERY_N_MILLISECONDS( 20 ) { gHue++; } // slowly cycle the "base color" through the rainbow
+  gPatterns[gCurrentPatternNumber]();
+  FastLED.show();
+  FastLED.delay(1000/FRAMES_PER_SECOND);
+  EVERY_N_MILLISECONDS( 20 ) { gHue++; } // slowly cycle the "base color" through the rainbow
 }
 
 void runner(CRGB clr)
@@ -83,10 +112,59 @@ void runner(CRGB clr)
   leds[pos] += clr;
 }
 
+void rainbow() 
+{
+  // FastLED's built-in rainbow generator
+  fill_rainbow( leds, NUM_LEDS, gHue, 7);
+}
+
+void rainbowWithGlitter() 
+{
+  // built-in FastLED rainbow, plus some random sparkly glitter
+  rainbow();
+  addGlitter(80);
+}
+
+void addGlitter( fract8 chanceOfGlitter) 
+{
+  if( random8() < chanceOfGlitter) {
+    leds[ random16(NUM_LEDS) ] += CRGB::White;
+  }
+}
+
 void confetti() 
 {
   // random colored speckles that blink in and fade smoothly
   fadeToBlackBy( leds, NUM_LEDS, 10);
   int pos = random16(NUM_LEDS);
   leds[pos] += CHSV( gHue + random8(64), 200, 255);
+}
+
+void sinelon()
+{
+  // a colored dot sweeping back and forth, with fading trails
+  fadeToBlackBy( leds, NUM_LEDS, 20);
+  int pos = beatsin16(13,0,NUM_LEDS);
+  leds[pos] += CHSV( gHue, 255, 192);
+}
+
+void bpm()
+{
+  // colored stripes pulsing at a defined Beats-Per-Minute (BPM)
+  uint8_t BeatsPerMinute = 62;
+  CRGBPalette16 palette = PartyColors_p;
+  uint8_t beat = beatsin8( BeatsPerMinute, 64, 255);
+  for( int i = 0; i < NUM_LEDS; i++) { //9948
+    leds[i] = ColorFromPalette(palette, gHue+(i*2), beat-gHue+(i*10));
+  }
+}
+
+void juggle() {
+  // eight colored dots, weaving in and out of sync with each other
+  fadeToBlackBy( leds, NUM_LEDS, 20);
+  byte dothue = 0;
+  for( int i = 0; i < 8; i++) {
+    leds[beatsin16(i+7,0,NUM_LEDS)] |= CHSV(dothue, 200, 255);
+    dothue += 32;
+  }
 }
