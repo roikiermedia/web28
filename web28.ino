5c3ca0248e04dab8af045e67959df5b3a36ae17f
Thorben Dittmar <roikiermedia@users.noreply.github.com>
2017-05-14 21:43:21 +0200

    Added basic web server with mDNS

--- web28.ino
+++ web28.ino
@@ -1,8 +1,10 @@
 #include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
 #include "FastLED.h"
 
-#define NETWORK_NAME ""
-#define NETWORK_PASSWORD ""
+#define NETWORK_NAME "Chaostreff-Flensburg"
+#define NETWORK_PASSWORD "Schnell33"
 
 #define DATA_PIN    6
 #define LED_TYPE    WS2812B
@@ -13,6 +15,12 @@ CRGB leds[NUM_LEDS];
 #define BRIGHTNESS         100
 #define FRAMES_PER_SECOND  60
 
+ESP8266WebServer server(80);
+
+
+void returnOK() {
+  server.send(200, "text/plain", "");
+}
 
 void setup() {
   Serial.begin(115200);
@@ -34,7 +42,17 @@ void setup() {
   Serial.print("Connected, IP address: ");
   Serial.println(WiFi.localIP());
 
-  for (int i=0; i<=NUM_LEDS*1.5; i++) {
+  if (MDNS.begin("web28")) {
+    Serial.println("MDNS responder started");
+  }
+
+  server.on("/", [](){
+    FastLED.showColor(0x0000FF);
+    server.send(200, "text/html", "Hello World!");
+  });
+  server.begin();
+
+  for (int i=0; i<=NUM_LEDS; i++) {
     runner(0x00FF00);
     FastLED.show();
     FastLED.delay(1000/FRAMES_PER_SECOND);
@@ -49,10 +67,12 @@ void setup() {
 uint8_t gHue = 0; // rotating "base color" used by many of the patterns
 
 void loop() {
-  confetti();
-  FastLED.show();
-  FastLED.delay(1000/FRAMES_PER_SECOND);
-  EVERY_N_MILLISECONDS( 20 ) { gHue++; } // slowly cycle the "base color" through the rainbow
+  server.handleClient();
+  
+  //confetti();
+  //FastLED.show();
+  //FastLED.delay(1000/FRAMES_PER_SECOND);
+  //EVERY_N_MILLISECONDS( 20 ) { gHue++; } // slowly cycle the "base color" through the rainbow
 }
 
 void runner(CRGB clr)
